6873f6f6ae15e7e1cfb54c0283359bb59eacb6a3
webintegrator <webintegrator@hotmail.com>
2016-05-31 23:03:01 -0400

    revision of week1 with collegue notes

--- node-express/dishRouter.js
+++ node-express/dishRouter.js
@@ -8,26 +8,41 @@ var bodyParser = require('body-parser');
 var dishRouter = express.Router();
 module.exports = dishRouter; //make it a module
 
+// If data contains json data. pasrse it
 dishRouter.use(bodyParser.json());
 
+
+// The route method is specified to be used.  The URL path is specified as '/'
+// The specific paths were the following dishRoute will be used is specified below
+// in app.use('/dishes',dishRouter)
 dishRouter.route('/')
+
+// 'all' is chained into 'route'
 .all(function(req,res,next) {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       next();
 })
 
+// 'get' is chained into 'route'
 .get(function(req,res,next){
         res.end('Will send all the dishes to you!');
 })
 
+
+// 'post' is chained into 'route'
 .post(function(req, res, next){
     res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
 })
 
+// 'delete' is chained into 'route' & ';' to end the chain of properties
 .delete(function(req, res, next){
         res.end('Deleting all dishes');
 });
 
+
+
+// define another router, to be applied if there is an id
+// same here, as above. Chain the actions together, and end with ';'
 dishRouter.route('/:dishId')
 .all(function(req,res,next) {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -50,5 +65,11 @@ dishRouter.route('/:dishId')
 
 //Module ends, All app.use and use.listen transfered to server.js,
 //the rest of the routers use pretty much this template but replacing the topic.
+
+// This server is set up to be REST server for dishes
+// Therefore, code is specified to handle all request actions (e.g. GET, PUT, DELETE, POST)
+// This will be set up for both dishes page and specific ID dishes
+// In this case we are using the Express Router, which allows a one-time defintion of the route,
+// rather than define it for each response situation
 
 
--- node-express/server.js
+++ node-express/server.js
@@ -1,4 +1,6 @@
 //NodeJS Dev. * Week1-Assignment1 * Miguel Estrada
+// All following notes taken from an assignment revision by a collegue
+
 
 var express = require('express');
 var morgan = require('morgan');
@@ -19,8 +21,26 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leadership',leaderRouter);
 
+// Make use of middle ware called 'static'.  This will be used to specify the location
+// of the static resources (ie html files).
+// The folder location will be used by the app (and 'dev) for determing responses
 app.use(express.static(__dirname + '/public'));
 
+
+// Use the app.listen function to start up the server
+// this is the short hand version of set up, no need for createServer commands
 app.listen(port, hostname, function(){
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+//load express router & morgan
+//Morgan is also loaded.  Morgan will performa number of dealut operations:
+//      - handle responss to requests for static resources
+//      - display to console info about type of request
+
+// Morgan will be used by express.  'dev' is a pre-formatted log output that morgan supports
+// Morgan default supports access to static resources, there only GET of existing files
+// There are inbuilt responses that will display based on the request commands.
+// If the file is found in the specified folder, the content is piped to the response.
+// If requested static resource cannot be found in specified folder, default error message is posted.
+// If a non GET message is sent as command, the default will recognise this and post error
